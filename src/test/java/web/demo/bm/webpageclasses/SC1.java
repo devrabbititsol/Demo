f2706a28cf88edb9800fa33afcf9ded9e1237835
santhosh-annam <santhosh.annam@devrabbit.com>
2019-01-28 10:20:30 +0530

    first commit

---
+++ src/test/java/web/demo/bm/webpageclasses/SC1.java
@@ -0,0 +1,33 @@
+package web.demo.bm.webpageclasses;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import com.utilities.BaseClass;import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+@SuppressWarnings("unused")
+public class SC1 extends BaseClass {
+	
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://devrabbitdev.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/UploadReportFile\",\"project_name\":\"Demo\",\"project_description\":\"desc\",\"project_id\":205,\"module_name\":\"BM\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":348,\"testcase_name\":\"TC1\",\"testcase_id\":412,\"testset_id\":0,\"executed_timestamp\":-1832166985,\"browser_type\":\"chrome\"}";
+
+	public static String projectName = "demo";
+	public WebDriver driver;
+	public ExtentReports reports;
+	public ExtentTest test;
+	public static final int datasetsLength = 1;
+
+	public SC1(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	@FindBy(how = How.XPATH, using = "//HTML/BODY[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/UL[1]/LI[2]/A[1]")	
+	private WebElement	Login_292259A;
+	public String clkALogin_292259() {
+		waitForExpectedElement(driver, Login_292259A);		
+		String text = Login_292259A.getText();
+		Login_292259A.click();
+		return text;
+	}
+
+}
---
+++ src/test/java/web/demo/bm/webtestclasses/TC1.java
@@ -0,0 +1,80 @@
+package web.demo.bm.webtestclasses;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.configurations.Constants;
+import com.configurations.ExtentConfigurations;
+import web.demo.bm.webpageclasses.SC1;
+import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.utilities.Utilities;
+import org.json.JSONObject;
+
+@SuppressWarnings("unused")
+public class TC1 extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	public boolean isElementDispalyed = false;	public static final int datasetsLength = 1;
+
+	public TC1() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(TC1.class);
+		configFileObj = new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("tc1.properties");
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath, SC1.projectName);
+		test = reports.startTest("TC1");
+	}
+	
+	public void setUP() throws Exception {
+		String primaryInfo = SC1.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		driver = launchBrowser(new JSONObject(primaryInfo).optString("browser_type"), configFileObj);
+	}
+
+	public void SC1Test(int i) throws Exception {
+
+	 try{
+		Constants.TOTAL_TC = Constants.TOTAL_TC + 1;
+		Constants.IS_TESTCASE = true; Constants.iS_WEB = true;
+		int datasetScreencount = 1;
+		SC1 objSC1 = PageFactory.initElements(driver, SC1.class);
+		testLogHeader(test, "Verify SC1 page");
+String text1 = objSC1.clkALogin_292259();
+		if(text1.equalsIgnoreCase(configFileObj.getProperty("Login"+ i + datasetScreencount))){
+			printSuccessLogAndReport(test, logger,  "Clicked on : " + configFileObj.getProperty("Login"+ i + datasetScreencount));
+			printSuccessLogAndReport(test, logger,  "Validated Link Text : " + configFileObj.getProperty("Login"+ i + datasetScreencount));
+		} else {
+			printFailureLogAndReport(test, logger,  "Link Text is not displayed  : " + configFileObj.getProperty("Login"+ i + datasetScreencount));
+		}
+
+	   } catch (Exception e) {
+		  isElementDispalyed = false;
+		  printFailureLogAndReport(test, logger,  "Element is not found" + e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void screensTest() throws Exception {
+		isElementDispalyed = true;
+		for(int datasets = 1; datasets <= SC1.datasetsLength; datasets++) {
+			if(!isElementDispalyed) return;			
+			setUP();
+			if(isElementDispalyed) { SC1Test(datasets);}
+			tearDown();
+		}	}
+
+	
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+		driver.quit();
+	}
+}
